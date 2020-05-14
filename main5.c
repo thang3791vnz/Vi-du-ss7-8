3dc8fc36d208c3e58c9fef9bc987fa40f97e1876
thang3791vnz <65003166+thang3791vnz@users.noreply.github.com>
2020-05-14 23:24:43 +0700

    viduss7,8

---
+++ main5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	if (result >= 45){
+		printf("Passed\n");
+			printf("Congratulations\n");
+	else
+			printf("Failed\n");
+			printf("Good luck next time\n");
+	}
+	return 0;
+}
